362a1828d6458b164756406820beda5ff2425829
Fahim Farhad <fahimfarhad@Fahims-MacBook-Air.local>
2025-01-11 21:04:49 +0600

    testing

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')).render(
     <Route element={<MainLayout />}>
     <Route index element={<Home />} />
     <Route path='about' element={<About />} />
-    {/* <Route path='about' element={<About />} /> */}
+    <Route path='about' element={<About />} />
     </Route>
     </Routes>
     </BrowserRouter>
