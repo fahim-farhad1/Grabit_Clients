9695d7e71db2842cb0fe43af17572e8343643b8f
Fahim Farhad <fahimfarhad@Fahims-MacBook-Air.local>
2025-01-13 16:44:03 +0600

    color theam added

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@
 const Navbar = () => {
   return (
     <div>
-      <p>This is Navbar</p>
+      
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#5CAF90',     
+        secondary: '#4B5966',  
+        accent: '#FF7070',  
+        rating: '#F7A465',    
+        background: '#FFFFFF',  
+        text: '#777777',    
+      },
+    },
   },
   plugins: [],
 }
